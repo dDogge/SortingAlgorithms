e44532b9886293e389b4e90c658f5c399d82e194
dDogge <douglasfjallrud@gmail.com>
2024-09-28 13:06:49 +0200

    Made it impossible to have multible sorting algorithms running at the same time

--- app/src/main/java/sortingalgorithms/SortingVisualizer.java
+++ app/src/main/java/sortingalgorithms/SortingVisualizer.java
@@ -144,48 +144,61 @@ public class SortingVisualizer implements ActionListener{
                     BubbleSort bs = new BubbleSort(list);
                     shuffleList.setEnabled(false);
                     selectAmount.setEnabled(false);
+                    selectAlg.setEnabled(false);
                     bs.Sort(visualList);
                     shuffleList.setEnabled(true);
                     selectAmount.setEnabled(true);
+                    selectAlg.setEnabled(true);
 
                 } else if (selected.equals("Cocktail Sort")) {
                     CocktailSort cs = new CocktailSort(list);
                     shuffleList.setEnabled(false);
                     selectAmount.setEnabled(false);
+                    selectAlg.setEnabled(false);
                     cs.Sort(visualList);
                     shuffleList.setEnabled(true);
                     selectAmount.setEnabled(true);
+                    selectAlg.setEnabled(true);
 
                 } else if (selected.equals("Selection Sort")) {
                     SelectionSort ss = new SelectionSort(list);
                     shuffleList.setEnabled(false);
                     selectAmount.setEnabled(false);
+                    selectAlg.setEnabled(false);
                     ss.Sort(visualList);
                     shuffleList.setEnabled(true);
                     selectAmount.setEnabled(true);
+                    selectAlg.setEnabled(true);
                     
                 } else if (selected.equals("Insertion Sort")) {
                     InsertionSort is = new InsertionSort(list);
                     shuffleList.setEnabled(false);
                     selectAmount.setEnabled(false);
+                    selectAlg.setEnabled(false);
                     is.Sort(visualList);
                     shuffleList.setEnabled(true);
                     selectAmount.setEnabled(true);
+                    selectAlg.setEnabled(true);
 
                 } else if (selected.equals("Bogo Sort")) {
                     BogoSort bs = new BogoSort(list);
                     shuffleList.setEnabled(false);
                     selectAmount.setEnabled(false);
+                    selectAlg.setEnabled(false);
                     bs.Sort(visualList);
                     shuffleList.setEnabled(true);
                     selectAmount.setEnabled(true);
+                    selectAlg.setEnabled(true);
+
                 } else if (selected.equals("Merge Sort")) {
                     MergeSort ms = new MergeSort(list);
                     shuffleList.setEnabled(false);
                     selectAmount.setEnabled(false);
-                    ms.Sort(visualList); 
+                    selectAlg.setEnabled(false);
+                    ms.Sort(visualList);
                     shuffleList.setEnabled(true);
                     selectAmount.setEnabled(true);
+                    selectAlg.setEnabled(true);
                 }
             }).start(); // Run sorting on a separate thread
         }
